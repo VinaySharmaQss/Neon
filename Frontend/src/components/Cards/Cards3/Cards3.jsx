f48ada722140697dd76fe2ce394a315d608d317f
Vinay Sharma <vinay.sharma@qsstechnosoft.com>
2025-01-30 17:48:29 +0530

    Cards3

---
+++ Frontend/src/components/Cards/Cards3/Cards3.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Cards3.module.css'
+const Cards3 = () => {
+  return (
+    <>
+    <div className={styles.container}>
+       <div className={styles.card}>
+        <img src="" alt="" />
+        <div className={styles.icons}>
+           <img src="" alt="" />
+        </div>
+       </div>
+    </div>
+    </>
+  )
+}
+
+export default Cards3
