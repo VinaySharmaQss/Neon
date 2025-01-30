fe6bcd3e6d1f3f273f4fbcd8bea1a0061b192e84
Vinay Sharma <vinay.sharma@qsstechnosoft.com>
2025-01-30 17:30:48 +0530

    Cards2

--- Frontend/src/components/Cards/Cards2/Cards2.jsx
+++ Frontend/src/components/Cards/Cards2/Cards2.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import styles from './Card2.module.css'
+import Image from "../../../assets/img/mountain.jpg"
 const Cards2 = () => {
   return (
-    <div>Cards2</div>
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.left_top}>
+          <img src={Image} alt="" />
+        </div>
+        <div className={styles.left_bottom}>
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <h2>
+        Indulge in the Finest Epicurean Cuisines
+        </h2>
+        <p className={styles.para1}>on Nov 17, 2022</p>
+        <p className={styles.para}>
+        Hi Charlie, we came to from our chef John that you didn't enjoyed the Epicurean cuisines yesterday. As a compensation we would like to offer you a free Italian cuisines as a goodwill gesture. Would you like to accept our request?
+        </p>
+        <div className={styles.btn_group}>
+          <button className={styles.btn_black}>Yes, I accept</button>
+          <button className={styles.btn_white}>No, thanks</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from '../../components/Navbar/Navbar';
 import GoodMorning from '../../components/MorningText/GoodMorning';
 import Slider from '../../components/Slider/Slider';
 import Cards1 from '../../components/Cards/Cards1/Cards1';
+import Cards2 from '../../components/Cards/Cards2/Cards2';
 const Home = () => {
   return (
     <>
@@ -15,6 +16,10 @@ const Home = () => {
     <main>
       <Slider>
         <Cards1/>
+      </Slider>
+
+      <Slider>
+        <Cards2/>
       </Slider>
     </main>
     </>
